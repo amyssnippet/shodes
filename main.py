e482c78c1514ffeb1f6c5c3469bc922a16b52d4e
Amol Yadav <amyssnipet@yahoo.com>
2025-08-21 18:28:59 +0000

    polished plots

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(filename='logs/shodes.log', level=logging.INFO, format='%(as
 logger = logging.getLogger()
 
 def main():
-    n_oscillators = 25
+    n_oscillators = 100
     k = 1.0
     kc = 0.1
     lambda_ = 0.1
